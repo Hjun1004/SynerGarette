6918763651b4cc9fa11c95a2cbbe06963a9aefca
Hjun1004 <hjun1004@naver.com>
2023-11-21 17:10:50 +0900

    프로젝ì트 세팅

---
+++ src/main/java/com/ll/synergarette/SynergaretteApplication.java
@@ -0,0 +1,13 @@
+package com.ll.synergarette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynergaretteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynergaretteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/synergarette/SynergaretteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.synergarette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynergaretteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
