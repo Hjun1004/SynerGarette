93348e77965d6cdd21b281ea3a6e842ed9e4a838
Hjun1004 <hjun1004@naver.com>
2023-12-11 22:02:27 +0900

    제품 등록 페이지에 ToastUI 에디터 적용 중

--- src/main/java/com/ll/synergarette/boundedContext/goods/entity/GoodsCreateForm.java
+++ src/main/java/com/ll/synergarette/boundedContext/goods/entity/GoodsCreateForm.java
@@ -1,6 +1,7 @@
 package com.ll.synergarette.boundedContext.goods.entity;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +16,12 @@ public class GoodsCreateForm {
     @NotBlank(message = "제목은 필수항목입니다.") // 아래의 변수는 비어있으면 안된다.
     @Size(max = 65, message = "제목을 200자 이하로 설정해주세요.") // 최대 200까지 가능하다.
     private String goodsName;
+
+    @Size(max = 100000, message = "제목을 100000자 이하로 설정해주세요.")
+    private String goodsDetail;
+
+    @NotNull(message = "가격은 필수항목입니다.")
+    private Long goodsPrice;
 
 
 }
